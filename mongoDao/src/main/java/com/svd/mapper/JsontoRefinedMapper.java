370410f724811a862226180e8acc96bac0b653f8
Alastair Miller <Alastir.Miller@veritas.com>
2018-03-09 17:36:05 +0000

    Created json mapper

---
+++ mongoDao/src/main/java/com/svd/mapper/JsontoRefinedMapper.java
@@ -0,0 +1,11 @@
+package com.svd.mapper;
+
+import org.bson.Document;
+import refinedDataModels.RefinedArtist;
+
+public class JsontoRefinedMapper {
+
+    public static RefinedArtist toArtist(Document document){
+
+    }
+}
